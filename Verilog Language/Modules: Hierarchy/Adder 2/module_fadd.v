4829ce0ad7c89b6747cca1b67926515c963d1336
Abhinav Sanal <112882054+AbhinavSanal@users.noreply.github.com>
2025-05-21 22:15:27 +0530

    Create module_fadd.v

---
+++ Verilog Language/Modules: Hierarchy/Adder 2/module_fadd.v
@@ -0,0 +1,17 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+	wire cc;
+    add16 add1(.a(a[15:0]),  .b(b[15:0]),  .cin(1'b0), .cout(cc), .sum(sum[15:0]));
+	add16 add2(.a(a[31:16]), .b(b[31:16]), .cin(cc),   .cout(),   .sum(sum[31:16]));
+
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+
+assign sum = a ^ b ^ cin;
+assign cout = a&b | a&cin | b&cin;
+endmodule
