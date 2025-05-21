bed7043957cd93f1b0dfbd8a186d8f92ba2a3651
Abhinav Sanal <112882054+AbhinavSanal@users.noreply.github.com>
2025-05-21 22:04:08 +0530

    Delete Verilog-Language/Modules: Hierarchy/Three-Module.v

--- Verilog-Language/Modules: Hierarchy/Three-Module.v
+++
@@ -1,6 +0,0 @@
-module top_module ( input clk, input d, output q );
-    wire [1:0] to;
-    my_dff dff1(.clk(clk), .d(d), .q(to[0]));
-    my_dff dff2(.clk(clk), .d(to[0]), .q(to[1]));
-    my_dff dff3(.clk(clk), .d(to[1]), .q(q));
-endmodule
