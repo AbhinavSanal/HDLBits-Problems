90f0561ddf15888650df1b98800dcc90b69835b8
Abhinav Sanal <112882054+AbhinavSanal@users.noreply.github.com>
2025-05-24 17:37:42 +0530

    Update dff8r.v

--- Circuits/Sequential Logic/Latches and Flip-Flops/dff8r.v
+++ Circuits/Sequential Logic/Latches and Flip-Flops/dff8r.v
@@ -4,24 +4,8 @@ module top_module (
     input [7:0] d,
     output [7:0] q
 );
-    genvar i;
-    generate
-        for(i = 0; i < 8; i++) begin : loop
-            dffr dff0(.clk(clk), .reset(reset), .d(d[i]), .q(q[i]));
-        end
-    endgenerate
-
-endmodule
-
-module dffr (
-    input clk,
-    input reset,
-    input d,
-    output q
-);
-    
     always @(posedge clk) begin
-        q <= reset ? 1'd0 : d;
+        q <= reset ? 8'd0 : d;
     end
 
 endmodule
