519a8a43f75c1378260f18f55e68a3276fa62890
Abhinav Sanal <112882054+AbhinavSanal@users.noreply.github.com>
2025-05-24 12:26:41 +0530

    Update lemmings4.v

--- Circuits/Sequential Logic/Finite State Machine/lemmings4.v
+++ Circuits/Sequential Logic/Finite State Machine/lemmings4.v
@@ -11,38 +11,39 @@ module top_module(
     output walk_right,
     output aaah,
     output digging ); 
-
+    
     parameter LEFT=0, RIGHT=1, FALL=2, DIG=3, SPLAT=4;
     reg [2:0] prev_state, state, next_state;
     reg [4:0] fall_count;
+    
+    always @(posedge clk or posedge areset) begin
+        if (areset) begin
+            state <= LEFT;
+            prev_state <= LEFT;
+        end else begin
+            // Store previous walking state before DIG or FALL
+            if(state == FALL & (next_state == FALL | next_state == SPLAT)) fall_count = fall_count + 1'd1;
+ 	        else fall_count = 5'd0;
+            if ((state == LEFT | state == RIGHT) & dig)
+                prev_state <= state;
+            state <= next_state;
+        end
+    end
     
     always @(*) begin
         case(state)
             LEFT: next_state = ground ? (dig ? DIG : (bump_left ? RIGHT : LEFT)) : FALL;
             RIGHT: next_state = ground ? (dig ? DIG : (bump_right ? LEFT : RIGHT)) : FALL;
-            FALL: next_state = ground ? ((fall_count > 20) ? SPLAT : prev_state) : FALL;
+            FALL: next_state = ground ? (fall_count < 5'd20 ? prev_state : SPLAT) : FALL;
             DIG: next_state = ground ? DIG : FALL;
             SPLAT: next_state = SPLAT;
         endcase
     end
-    
-    always @(posedge clk, posedge areset) begin
-        if(areset) state = LEFT;
-        else begin
-                if((state == LEFT | state == RIGHT) & (dig | ~ground))
-                    prev_state = state;
-        end
-        state = next_state;
-    end
    
-    always @(posedge clk) begin
-        if(state == FALL & next_state == FALL) fall_count = fall_count + 1;
-        else fall_count = 0;
-    end
-    
     assign walk_left = (state == LEFT) ? 1'b1 : 1'b0;
     assign walk_right = (state == RIGHT) ? 1'b1 : 1'b0;
-    assign aaah = (state == FALL) ? 1'b1 : 1'b0;
-    assign digging = (state == DIG) ? 1'b1 : 1'b0; 
-    
+    assign aaah = ~walk_left & ~walk_right & ~digging;
+    assign digging = (state == DIG) ? 1'b1 : 1'b0;
+
 endmodule
+
